0f04eaa539370dbc2c655353d7e8b08acfb7bd6b
Dexter White <dexter.w.white12@gmail.com>
2024-11-23 15:01:10 -0600

    Spring Boot Initializr Files

---
+++ everything/src/main/java/com/usagi/everything/EverythingAppApplication.java
@@ -0,0 +1,13 @@
+package com.usagi.everything;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverythingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EverythingAppApplication.class, args);
+	}
+
+}
---
+++ everything/src/test/java/com/usagi/everything/EverythingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usagi.everything;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverythingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
